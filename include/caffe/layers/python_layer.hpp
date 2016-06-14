2e43c863c9fa08c0e487dad2e3f38b7fa9bcc906
kukuruza <toropov.evgeny@gmail.com>
2016-06-14 09:47:36 -0700

    Fixed remaining merging errors
    1) add set_random_seed into python/caffe/__init__.py
    2) removed extra set_random_seed from python/caffe/_caffe.cpp
    3) removed phase allocation in include/layers/python_layer.hpp

--- include/caffe/layers/python_layer.hpp
+++ include/caffe/layers/python_layer.hpp
@@ -26,7 +26,12 @@ class PythonLayer : public Layer<Dtype> {
     }
     self_.attr("param_str") = bp::str(
         this->layer_param_.python_param().param_str());
-    self_.attr("phase") = static_cast<int>(this->phase_);
+
+    //LOG(INFO) << "Python desired phase is: " << (static_cast<int>(this->phase_) == 0 ? "TRAIN" : "TEST");
+    //LOG(INFO) << "Python real phase is:    " << (self_.attr("phase") == caffe::TRAIN ? "TRAIN" : "TEST");
+    //self_.attr("phase") = (static_cast<int>(this->phase_) == 0 ? caffe::TRAIN : caffe::TEST);
+    //self_.attr("phase") = static_cast<caffe::Phase>(this->phase_);
+
     self_.attr("setup")(bottom, top);
   }
   virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
