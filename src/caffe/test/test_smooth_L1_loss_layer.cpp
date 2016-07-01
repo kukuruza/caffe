ab67ea94b5b1078f0150717c9cc48e6c57cc76da
Evgeny Toropov <toropov.evgeny@gmail.com>
2016-07-01 14:42:45 -0400

    moved faster_rcnn_layers.hpp to layers dir and split into two headers

--- src/caffe/test/test_smooth_L1_loss_layer.cpp
+++ src/caffe/test/test_smooth_L1_loss_layer.cpp
@@ -8,8 +8,7 @@
 #include "caffe/blob.hpp"
 #include "caffe/common.hpp"
 #include "caffe/filler.hpp"
-#include "caffe/vision_layers.hpp"
-#include "caffe/fast_rcnn_layers.hpp"
+#include "caffe/layers/smooth_L1_loss_layer.hpp"
 
 #include "caffe/test/test_caffe_main.hpp"
 #include "caffe/test/test_gradient_check_util.hpp"
